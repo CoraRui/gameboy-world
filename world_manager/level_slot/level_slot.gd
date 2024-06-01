b5122b7d38f78302de5c49b281ccc4a2bc3f513f
cora rui <coraruidev@gmail.com>
2024-05-31 19:47:34 -0700

    initial commit

---
+++ world_manager/level_slot/level_slot.gd
@@ -0,0 +1,7 @@
+extends Node
+class_name level_slot
+
+@export var level_res : level 
+
+
+#holds info for a single level in world manager
---
+++ world_manager/world_manager/world_manager.gd
@@ -0,0 +1,6 @@
+extends Node2D
+
+#should contain references to worlds in children.
+#gives the world to some kind of loader.
+
+
